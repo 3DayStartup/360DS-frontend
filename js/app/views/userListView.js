46eea83bc61fbf5b654ecc29c07916fc7c7090fe
Jonathan Rosenberg <jon@jackrabbitmobile.com>
2013-02-24 19:59:56 -0600

    Got faces showing up

--- js/app/views/userListView.js
+++ js/app/views/userListView.js
@@ -4,6 +4,7 @@ function($, _, Backbone, UserList, UserModel) {
 	var UserListView = Backbone.View.extend({
 		
 		el : '#user-list',
+		tagName: 'ul',
 		
 		template : _.template($("#user-list-template").html()),
 
@@ -15,16 +16,17 @@ function($, _, Backbone, UserList, UserModel) {
 
 		render : function() {
 			console.log('user boad view render...');
-			//var attributes = this.model.toJSON();
-			//this.$el.html(this.template(attributes));
-			//this.input = this.$('.edit');
-			//console.log('[rendered]');
+			this.$el.html("")
 			this.collection.forEach(this.addOne, this);
+			
 			return this;
 		},
 		
 		addOne: function(userModel) {
 			console.log("userListView.addone");
+			var attributes = userModel.toJSON();
+			this.$el.prepend(this.template(attributes));
+			console.dir($(this.$el));
 			//var userView = new UserView({model : userModel});
 			//this.$el.append(userView.render().el);
 			return this;
