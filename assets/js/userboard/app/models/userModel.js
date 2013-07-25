b1668fb17d2b8191879c39fe4bfa580f2c1d64d5
jonrosenberg <jon.rosenberg@gmail.com>
2013-07-25 00:32:04 -0500

    Added new 3ds programs
    3DS St Marys Summer 2013
    3DS Bogota Summer 2013
    3DS Austin Fall 2013
    3DS San Antonio Summer 2013
    3DS Georgia Tech Fall 2013

--- assets/js/userboard/app/models/userModel.js
+++ assets/js/userboard/app/models/userModel.js
@@ -8,7 +8,7 @@ define(["jquery", "underscore", "backbone",  'backbone_forms'], function($, _, B
         email:      		{ validators: ['required', 'email'] },
         profilePicture: 	'Text',
         company: 		'Text',
-        program: 		{ type: 'Select', options: ['', '3DS RetailMeNot Spring 2013', '3DS University of Houston Spring 2013', '3DS TAMU Spring 2013'], validators: ['required'] },
+        program: 		{ type: 'Select', options: ['', '3DS RetailMeNot Spring 2013', '3DS University of Houston Spring 2013', '3DS TAMU Spring 2013', '3DS St Marys Summer 2013', '3DS Bogota Summer 2013', '3DS Austin Fall 2013', '3DS San Antonio Summer 2013', '3DS Georgia Tech Fall 2013'], validators: ['required'] },
         fieldOfStudy: 		'Text',
         degreeProgram:		{ type: 'Select', options: ['High School', 'Associate', 'Bachelor', 'Master', 'Doctorate', 'Juris Doctorate'] },
         university: 		'Text',
