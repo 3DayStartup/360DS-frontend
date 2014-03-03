60bbe9f69db03328444421ea8aad13c60c441e07
Fernando Romero <fernando.romero@rightscale.com>
2014-03-02 23:30:33 -0500

    userboard-node replaced by userboard-grunt

--- userboard-node/app/collections/users.js
+++
@@ -1,9 +0,0 @@
-/* global App:true, Backbone:true */
-
-App.Collections.Users = Backbone.Firebase.Collection.extend({
-	model: App.Models.User,
-
-	initialize: function(models, options){
-		this.firebase = options.firebase;
-	}
-});
